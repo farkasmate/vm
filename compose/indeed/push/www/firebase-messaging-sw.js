fc5c8306bf9c5d980e3b06e8054af395d536ed5b
Mate Farkas <mate.farkas@sch.hu>
2020-04-19 12:44:45 +0200

    Remove push.

--- compose/indeed/push/www/firebase-messaging-sw.js
+++
@@ -1,9 +0,0 @@
-importScripts('https://www.gstatic.com/firebasejs/5.5.7/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/5.5.7/firebase-messaging.js');
-
-firebase.initializeApp({
-  messagingSenderId: "841751645592"
-});
-
-const messaging = firebase.messaging();
-
